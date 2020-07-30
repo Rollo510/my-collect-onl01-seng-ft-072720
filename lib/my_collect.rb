edbc9d0a2f95c6a899e6bf3bc1fce7831360dd2f
Kevin Britt <kbritt510@gmail.com>
2020-07-30 05:40:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,34 @@
+def collect(collection)
+  
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
